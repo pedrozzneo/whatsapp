552b8dcae5cf746f069e7232687a9a9ac1ab6988
pedro <pedrozzneo@gmail.com>
2025-05-13 20:53:29 -0300

    organize: create a set_driver function

--- whatsapp.py
+++ whatsapp.py
@@ -9,22 +9,25 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
-# Create chrome options object
-chrome_options = Options()
+def set_driver():
+    # Create chrome options object
+    chrome_options = Options()
 
-# Set the options for Chrome
-selenium_data_dir = os.path.join(os.path.expanduser('~'), 'selenium-chrome-data') 
-chrome_options.add_argument(f'user-data-dir={selenium_data_dir}')
-chrome_options.add_argument('--profile-directory=Default')
-chrome_options.add_argument("--disable-notifications")
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--disable-notifications')
-chrome_options.add_argument('--disable-gpu')  
-chrome_options.add_argument('--start-maximized')
+    # Set the options for Chrome
+    selenium_data_dir = os.path.join(os.path.expanduser('~'), 'selenium-chrome-data') 
+    chrome_options.add_argument(f'user-data-dir={selenium_data_dir}')
+    chrome_options.add_argument('--profile-directory=Default')
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-notifications')
+    chrome_options.add_argument('--disable-gpu')  
+    chrome_options.add_argument('--start-maximized')
 
+    return webdriver.Chrome(options=chrome_options)
+
+driver = set_driver()
 # Open whatsapp
-driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://web.whatsapp.com")
 
 # Set up the wait time for EC
